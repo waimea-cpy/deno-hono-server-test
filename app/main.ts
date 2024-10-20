09e90a7a009b7b8d163c70430a2df8a9c1168619
Steve Copley <copley.steve@gmail.com>
2024-10-20 22:20:06 +1300

    Setting up for Eta templates

--- app/main.ts
+++ app/main.ts
@@ -3,18 +3,25 @@ import { html, raw } from 'jsr:@hono/hono/html'
 import { serveStatic } from 'jsr:@hono/hono/deno'
 import { DB } from "https://deno.land/x/sqlite@v3.9.1/mod.ts"
 import type { HtmlEscapedString } from "jsr:@hono/hono/utils/html";
+import { Eta } from "jsr:@eta-dev/eta";
 
 
 // Open a database
 const db = new DB('db/test.db');
 
+// Setup template engine
+const viewPath = Deno.cwd()+'/views/'
+const eta = new Eta({ views: viewPath, cache: true })
 
+
+// Page header component
 const Header = (title: string) => html`
     <header>
         <h1>${title}</h1>
     </header>
 `
 
+// Page nav component
 const Nav = () => html`
     <nav>
         <a href="/">Home</a>
@@ -23,12 +30,14 @@ const Nav = () => html`
     </nav>
 `
 
+// Page footer component
 const Footer = () => html`
     <footer>
         &copy; 2024
     </footer>
 `
 
+// Default page layout
 const Layout = (
     title: string,
     content: HtmlEscapedString | Promise<HtmlEscapedString> = html``
@@ -51,14 +60,17 @@ const Layout = (
     </html>
 `
 
+// Create new routing engine
 const app = new Hono()
 
+// Home page
 app.get('/', (c: Context) => {
     return c.html(
         Layout('Hello, World!')
     )
 })
 
+// Hello page
 app.get('/hello', (c: Context) => {
     return c.html(
         Layout(
@@ -68,6 +80,7 @@ app.get('/hello', (c: Context) => {
     )
 })
 
+// All users
 app.get('/users', (c: Context) => {
     const records = db.query('SELECT id, name FROM people')
     console.log(records)
@@ -95,6 +108,7 @@ app.get('/users', (c: Context) => {
     )
 })
 
+// Specific single user (via id)
 app.get('/users/:id', (c: Context) => {
     const id = c.req.param('id')
 
@@ -118,7 +132,9 @@ app.get('/users/:id', (c: Context) => {
     }
 })
 
-app.use('/static/*', serveStatic({ root: './' }))
+// All static files (e.g. images / css)
+app.use('/static/*', serveStatic({ root: Deno.cwd() }))
 
 
+// Launch server
 Deno.serve(app.fetch)
